ab74f9d6253f630e5c658839425b9d9a916a014d
Chris McDonough <chrism@plope.com>
2011-07-22 02:44:02 -0400

    provide a tox.ini for easier testing; cope with setup.py stupidity in generating long_description

--- setup.py
+++ setup.py
@@ -18,8 +18,12 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = ''
+    CHANGES = ''
 
 setup(name='pyramid_who',
       version=__version__,
